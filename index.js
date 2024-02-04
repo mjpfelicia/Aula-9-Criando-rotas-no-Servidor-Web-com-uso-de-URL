93a8075d4a8de467db3c131a93806d504658ae8c
mjpfelicia <86963473+mjpfelicia@users.noreply.github.com>
2024-02-04 12:22:12 +0000

    criando rotas

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const httptest = require("http");
+const http = require("http");
+const port = 443;
+const url = require("url");
+
+const servidor = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(req.url);
+
+  const parametro = url.parse(req.url, true).query;
+
+  res.write("<br>Nome: " + parametro.nome);
+  res.write("<br>Sobrenome: " + parametro.sobrenome);
+  res.write("<br>idade: " + parametro.idade);
+
+  res.end();
+});
+servidor.listen(port, () => {
+  console.log("servidor rodando");
+});
